5940231edae0145fcb9a52b483160e58fef02362
I565171 <g.sowjanya@sap.com>
2024-10-13 17:44:12 +0530

    step component changes

--- steps/src/App-v1.js
+++ steps/src/App-v1.js
@@ -10,6 +10,7 @@ function App(){
   return(
     <div>
       <Step/>
+      <Step />
     </div>
   )
 }
@@ -37,39 +38,16 @@ function Step(){
         <div className={`${step >= 3 ? "active" : ""}`}>3</div>
       </div>
 
-    <StepMessage step={step}>
-      {messages[step-1]}
-    </StepMessage>
+      <p className="message">Step {step}: {messages[step-1]}</p>
 
       <div className="buttons">
-            <Button textColor="#fff" bgColor="#7950f2" onClick={handlePrevious}> 
-              <span>👈</span>Previous
-            </Button>
-            <Button textColor="#fff" bgColor="#7950f2" onClick={handleNext}>
-              Next <span>👉</span>
-            </Button>
+         <button style={{backgroundColor:"#7950f2",color:"#fff"}} onClick={handlePrevious}>Previous</button>
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }} onClick={handleNext}>Next</button>
       </div>
     </div>
 }
     </>
   )
-}
-
-function StepMessage({step,children}){
-  return(
-  <p className="message">
-    <h3>Step {step}</h3>
-    {children}
-  </p>
-  )
-}
-
-function Button({textColor,bgColor,onClick,children}){
-   return(
-     <button style={{ backgroundColor: bgColor, color:textColor }} onClick={onClick}>
-      {children}
-    </button>
-   )
 }
 
 export default App;
